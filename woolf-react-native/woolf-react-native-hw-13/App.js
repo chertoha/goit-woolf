3043bef6f5951142876b2d3baa58e33432980f82
Anton <a.chertok@akatech.com.ua>
2025-03-16 13:51:16 +0000

    woolf-react-native-hw-13

---
+++ woolf-react-native/woolf-react-native-hw-13/App.js
@@ -0,0 +1,39 @@
+import "react-native-gesture-handler";
+import { Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import StackNavigator from "./navigation/StackNavigator";
+
+SplashScreen.preventAutoHideAsync();
+const MainStack = createStackNavigator();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <>
+      <StackNavigator />
+    </>
+  );
+}
